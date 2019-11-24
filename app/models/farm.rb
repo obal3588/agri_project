f5cf5215b4158b874d452871d719f8b163bf121d
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-11-24 19:13:28 +0300

    seeds added

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,7 +1,3 @@
 class Farm < ApplicationRecord
-    has_many :fields, , dependent: :destroy
-<<<<<<< HEAD
-end
-=======
+    has_many :fields, dependent: :destroy
     end
->>>>>>> 1a731367059a26e89c7ec087ffd9b1f719fe37b0
