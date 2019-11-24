d28b2df0c0eaedb5519f1433e4511664146d3da9
Abdullah Rashed <abdullah_rashed.a@icloud.com>
2019-11-24 16:36:24 +0300

    update_farms_controller.rb

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -1,14 +1,3 @@
 class Farm < ApplicationRecord
     has_many :fields, , dependent: :destroy
-    def  index 
-        @farms = Fram.all
     end
-    def new
-        @farm = Farm.new
-    end
-    def destroy
-        Farm.find(params[:id]).destroy
-        redirect_to farms_path
-    end
-    
-end
