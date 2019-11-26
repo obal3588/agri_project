0119cb0865973b8288915515d22bffa99ad18a14
obal3588 <obal3588@colorado.edu>
2019-11-26 17:23:06 +0300

    Done ver1

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Farm.create(name: "farm 1", location: "Al-dilam")
+Farm.create(name: "mr.x farms", location: "Al-khobar")
+Farm.create(name: "farm 3", location: "Riyadh")
+Farm.create(name: "farm 4", location: "Banban")
 
-Farm.create(name: 'farm 1' , location:'Al-dilam')
-Farm.create(name: 'mr.x farms' , location:'Al-khobar')
-Farm.create(name: 'farm 3' , location:'Riyadh')
-Farm.create(name: 'farm 4' , location:'Banban')
+Plot.create(name: "plot A", crop: "Pumpkin", farm_id: 1)
+Plot.create(name: "x", crop: "Carrot", farm_id: 2)
+Plot.create(name: "plot h", crop: "Cucumber", farm_id: 1)
+Plot.create(name: "d", crop: "okra", farm_id: 2)
+Plot.create(name: "plot z", crop: "pumpkin", farm_id: 1)
 
-
-Plot.create(name: 'plot A', crop:'Pumpkin', farm_id: 1)
-Plot.create(name: 'x', crop:'Carrot', farm_id: 2)
-Plot.create(name: 'plot h', crop:'Cucumber', farm_id: 1)
-Plot.create(name: 'd', crop:'okra', farm_id: 2)
-Plot.create(name: 'plot z', crop:'pumpkin', farm_id: 1)
+User.create! :email => "admin@gmail.com", :password => "123123", :password_confirmation => "123123", :admin => true
+User.create! :email => "emp@gmail.com", :password => "123123", :password_confirmation => "123123", :emp => true
+User.create! :email => "user@gmail.com", :password => "123123", :password_confirmation => "123123", :admin => true
